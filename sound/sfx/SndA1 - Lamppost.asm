4e2544cd282aef4f487d6009099bc87f78f66ec4
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-08 07:28:37 +0300

    sound effect

--- sound/sfx/SndA1 - Lamppost.asm
+++ sound/sfx/SndA1 - Lamppost.asm
@@ -4,30 +4,13 @@ SndA1_Lamppost_Header:
 	smpsHeaderTempoSFX  $01
 	smpsHeaderChanSFX   $01
 
-	smpsHeaderSFXChannel cFM5, SndA1_Lamppost_FM5,	$00, $01
+	smpsHeaderSFXChannel cPSG1, SndA1_Lamppost_PSG1,	$00, $01
 
-; FM5 Data
-SndA1_Lamppost_FM5:
-	smpsSetvoice        $00
-	dc.b	nC5, $06, nA4, $16
+; PSG1 Data
+SndA1_Lamppost_PSG1:
+	dc.b	nG3, $05
+	smpsModSet          $02, $01, $01, $F0
+	dc.b	nG4, $30
 	smpsStop
 
 SndA1_Lamppost_Voices:
-;	Voice $00
-;	$3C
-;	$05, $01, $0A, $01, 	$56, $5C, $5C, $5C, 	$0E, $11, $11, $11
-;	$09, $0A, $06, $0A, 	$4F, $3F, $3F, $3F, 	$17, $80, $20, $80
-	smpsVcAlgorithm     $04
-	smpsVcFeedback      $07
-	smpsVcUnusedBits    $00
-	smpsVcDetune        $00, $00, $00, $00
-	smpsVcCoarseFreq    $01, $0A, $01, $05
-	smpsVcRateScale     $01, $01, $01, $01
-	smpsVcAttackRate    $1C, $1C, $1C, $16
-	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $11, $11, $11, $0E
-	smpsVcDecayRate2    $0A, $06, $0A, $09
-	smpsVcDecayLevel    $03, $03, $03, $04
-	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $00, $20, $00, $17
-
